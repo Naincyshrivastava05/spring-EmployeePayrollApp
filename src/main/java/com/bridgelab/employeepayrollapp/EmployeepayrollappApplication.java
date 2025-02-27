f7f0c31b0365c2fc7f1914a70fbf46a9cd18e82d
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-27 16:21:45 +0530

    [Naincy] add Determine the Logging Levels

--- src/main/java/com/bridgelab/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/bridgelab/employeepayrollapp/EmployeepayrollappApplication.java
@@ -3,7 +3,10 @@ package com.bridgelab.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class  EmployeepayrollappApplication {
 
 	public static void main(String[] args) {
