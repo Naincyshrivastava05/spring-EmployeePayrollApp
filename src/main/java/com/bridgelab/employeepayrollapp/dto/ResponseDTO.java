a53379d02399b6ac6118d87aa26a0f51d26ecd06
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-27 16:03:55 +0530

    [Naincy] add lombok library to auto generated getters and setters for the DTO

--- src/main/java/com/bridgelab/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelab/employeepayrollapp/dto/ResponseDTO.java
@@ -2,7 +2,8 @@ package com.bridgelab.employeepayrollapp.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-
+import lombok.Data;
+@Data
 public class ResponseDTO {
     @NotEmpty(message = "Name cannot be empty")
     @Pattern(regexp ="[A-Z][a-zA-Z\\s]+$", message = "Name should start with capital letter")
